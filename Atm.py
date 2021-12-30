b6ffe19ed676adb64553b0633fc5dd6935d67a97
Topden1 <87784907+Topden1@users.noreply.github.com>
2021-12-30 09:57:15 -0800

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class Atm:
+    def __init__(self, cardnumber, pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+        def check_balance(self)
+            print("Your balance is 50000")
+
+         new_user = Atm(Card_number, pin_number)
+            
